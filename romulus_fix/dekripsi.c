f7ecdedf053072af19e9b96004ddccc89cbdcb7c
Rafli029 <97864319+Rafli029@users.noreply.github.com>
2023-05-23 16:01:15 +0700

    Create dekripsi.c

---
+++ romulus_fix/dekripsi.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "api.h"
+#include "crypto_aead.h"
+#include "romulus_m.h"
+#include "skinny.h"
+#include "variant.h"
+
+#define MAX_MSG_LENGTH 1024
+#define AD_BLK_LEN_EVN 16
+
+int main() {
+    unsigned char key[CRYPTO_KEYBYTES];
+    unsigned char nonce[CRYPTO_NPUBBYTES];
+    unsigned char plaintext[MAX_MSG_LENGTH];
+    unsigned char ciphertext[MAX_MSG_LENGTH + CRYPTO_ABYTES];
+    unsigned char ad[AD_BLK_LEN_EVN] = "1234";
+    unsigned long long plaintext_len, ad_len, ciphertext_len;
+
+    // Inisialisasi kunci dan nonce
+    memset(key, 0, sizeof(key));
+    memset(nonce, 0, sizeof(nonce));
+
+    ad_len = strlen((char*) ad) - 1;
+    ad[ad_len] = '\0';
+
+    // Input ciphertext
+    printf("Masukkan ciphertext: ");
+    char hex[MAX_MSG_LENGTH * 2 + 1];
+    fgets(hex, sizeof(hex), stdin);
+    ciphertext_len = (strlen(hex));
+    hex[ciphertext_len-1] = '\0';
+
+    // Konversi dari hexadecimal ke binary
+    for (int i = 0; i < ciphertext_len; i += 2) {
+        sscanf(hex + i, "%2hhx", ciphertext + i / 2);
+    }
+
+    // Dekripsi ciphertext
+    if (crypto_aead_decrypt(plaintext, &plaintext_len, NULL, ciphertext, ciphertext_len / 2, ad, ad_len, nonce, key) != 0) {
+        printf("Dekripsi gagal\n");
+        return 1;
+    }
+
+    printf("Plaintext: %s\n", plaintext);
+
+    return 0;
+}
